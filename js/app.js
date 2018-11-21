ada0f5a7509cf82d58c45b9a53bd0f576d47c9bb
Ferran Espasa <fespasa@me.com>
2018-11-21 19:05:44 +0100

    Search Form Creation

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	
+	alert('Document is ready!');
+	
+	/* ------------- Search Markup Creation ------------- */
+	$('.search-container').append('<form action="#" method="get"><input type="search" id="search-input" class="search-input" placeholder="Search..."><input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit"></form>');
+	
+});
