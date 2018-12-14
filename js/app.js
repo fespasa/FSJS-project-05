f5749af26838a94ac39c19e40c50e002de3fdac6
Ferran Espasa <fespasa@me.com>
2018-12-14 13:30:24 +0100

    prev & next buttons added to modals

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	
 	/* ------------- Display Cards ------------- */
 	var randomUserAPI = "https://randomuser.me/api/?results=12";
-	
+
 	$.getJSON(randomUserAPI, function(response){
 		console.log(response);
 		console.log($.type(response));
@@ -27,14 +27,16 @@ $(document).ready(function(){
 
 			$('#gallery').append($cardHTML);
 			
+						
 			var $cardModal = '<div class="modal-container" id="modal-'+ index +'"><div class="modal"><button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button><div class="modal-info-container">';
 			$cardModal += '<img class="modal-img" src="'+ value.picture.medium +'" alt="profile picture">';
 			$cardModal += '<h3 id="name" class="modal-name cap">'+ value.name.first + ' ' + value.name.last +'</h3>';
 			$cardModal += '<p class="modal-text">'+ value.email +'</p>';
-			$cardModal += '<p class="modal-text cap">'+ value.location.city +'</p><hr>';
-			$cardModal += '<p class="modal-text">'+ value.phone +'</p>';
+			$cardModal += '<p class="modal-text cap">'+ value.location.city +', '+ value.location.state +'</p><hr>';
+			$cardModal += '<p class="modal-text">'+ value.cell +'</p>';
 			$cardModal += '<p class="modal-text">'+ value.location.street +'</p>';
 			$cardModal += '<p class="modal-text">Birthday: '+ value.dob.date +'</p>';
+			$cardModal += '<button type="button" name="Prev" class="modal-prev-btn" style="padding= 5px; margin= 10px;">Prev</button><button type="button" class="modal-next-btn" style="padding= 5px; margin= 10px;">Next</button>'
 			$cardModal += '</div></div>';
 			
 			$('body').append($cardModal);
@@ -42,7 +44,7 @@ $(document).ready(function(){
 			$('.modal-close-btn').click(function(){
 				$('.modal-container').css('display', 'none');
 			});
-				
+							
 			console.log(index);
 			
 		}); // end each
@@ -51,7 +53,7 @@ $(document).ready(function(){
 		
 		$.each($('.card'), function(index, card){
 			console.log("I'm on card nº "+ index);
-			$('#card-'+index).click(function(){
+			$(this).on("click", function(){
 				console.log(card);
 				var modal = $('#modal-'+index);
 				console.log(modal);
@@ -59,7 +61,37 @@ $(document).ready(function(){
 			});
 		});
 		
+		$.each($('.modal-prev-btn'), function(index, button){
+			$(this).on("click", function(){
+				console.log("Prev Button Pressed");
+				$('.modal-container').css('display', 'none');
+				if(index === 0){
+					var modal = $('#modal-11');
+				} else {
+					var prevIndex = index - 1;
+					var modal = $('#modal-'+ prevIndex);
+				}
+				modal.css('display', 'block');
+			});
+		});
 		
-	});
+		$.each($('.modal-next-btn'), function(index, button){
+			$(this).on("click", function(){
+				console.log("Next Button Pressed");
+				$('.modal-container').css('display', 'none');
+				if(index === 11){
+					var modal = $('#modal-0');
+				} else {
+					var nextIndex = index + 1;
+					var modal = $('#modal-'+ nextIndex);
+				}
+				modal.css('display', 'block');
+			});
+		});
+
+	}); // end getJSON
 	
-});
+	
+		
+		
+}); // end ready
